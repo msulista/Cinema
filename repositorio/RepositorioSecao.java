ac6a0cdc110bd48cfa94ec95339b0a2b650c7915
msulista <marcus.rodrigues81@gmail.com>
2015-03-29 20:42:43 -0300

    Criando Classe RepositorioSecao

---
+++ repositorio/RepositorioSecao.java
@@ -0,0 +1,60 @@
+package repositorio;
+
+import model.Secao;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by marcus.rodrigues on 29/03/2015.
+ */
+public class RepositorioSecao {
+    
+    private List<Secao> secoes;
+
+    public RepositorioSecao() {
+        this.secoes = new ArrayList<Secao>();
+    }
+    
+    public boolean adicionaSecao(Secao secao){
+        return (secoes.add(secao));
+    }
+    public List<Secao> getSecoes(){
+        return secoes;
+    }
+    
+    public boolean verificaSeJaExisteSecaoPorTituloDoFilme(String titulo){
+        for (Secao secao : secoes){
+            if(secao.getFilme().getTitulo().equalsIgnoreCase(titulo)){
+                return (true);
+            }
+        }
+        return (false);
+    }
+    public Secao buscaSecaoPorTituloDoFilme(String titulo){
+        for (Secao secao : secoes){
+            if (secao.getFilme().getTitulo().equalsIgnoreCase(titulo)){
+                return secao;
+            }
+        }
+        return null;
+    }
+    
+    public boolean verificaSeJaExisteSecaoPorHorario(Date horario){
+        for (Secao secao : secoes){
+            if (secao.getHorario().equals(horario)){
+                return (true);
+            }
+        }
+        return (false);            
+    }
+    public  Secao buscaSecaoPorHorario(Date horario){
+        for (Secao secao : secoes){
+            if (secao.getHorario().equals(horario)){
+                return secao;
+            }
+        }
+        return null;
+    }
+}
